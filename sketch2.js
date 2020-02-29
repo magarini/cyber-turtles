48bd8ef5d3f0813e71189f44c360c8e2d18a0888
magarini <56253774+magarini@users.noreply.github.com>
2020-02-29 16:01:17 +0200

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -3,12 +3,11 @@ let coral;
 let bubbles=[];
 let bubble
 let ear = document.getElementById('ear');
-
+let j=0;
 
 function preload() {
     // Load model with normalise parameter set to true
     coral = loadModel('10042_Sea_Turtle_V2_iterations-2.obj', true);
-    //img = loadImage('10042_Sea_Turtle_V1_Diffuse.jpg');
     console.log('loaded')
     img = loadImage('underwaterPanorama1.jpg');
     glitter = loadImage('outerspace.jpg');
@@ -18,36 +17,27 @@ function preload() {
 
 function setup(){
     let canvas= createCanvas(windowWidth,windowHeight,WEBGL);
-    //textureMode(IMAGE);
-    //textureWrap(MIRROR);
    
-   //ortho(-width, width, height, -height/2, 0.1, 100);
   }
 
 
   function draw(){
     clear();
-    //background(0);
     angleMode(DEGREES);
    ambientLight(255);
  
     pointLight(255,mouseX,mouseY);
-  //directionalLight(255,600,200,0);
-  // rotateX(200);
+
   noStroke();
   texture(img);
   sphere(1000);
     normalMaterial();
-   //specularMaterial(255, 10, 50);
-//    texture(img);
-//shininess(10);
+
 orbitControl();
 rotateX(30);
 rotateX(frameCount * 0.05);
 texture(glitter);
-//textureWrap(CLAMP);
 
-//let x=random(5);
 push();
 translate(0,0);
 rotateY(frameCount * -0.01);
@@ -61,7 +51,7 @@ push();
     pop();
 
     push();
-    translate(-200,100);
+    translate(-200,200);
     rotateX(frameCount * -0.02);
 model(coral);
 pop();
@@ -87,15 +77,14 @@ push();
     pop();
    
 
-    // translate(200,500)
-    // model(coral);
-// ear.mousePressed();
   }
 
   function mousePressed(){
+    j=j+1;
+    if (j<2){
     mySound.setVolume(0.4);
-    mySound.play();
-    
+    mySound.loop();
+  }
   }
 
   
